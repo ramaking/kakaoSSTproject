2749d0a449ee223016f0b7c6c372bd1311c10df0
ramaking <aparter01@naver.com>
2022-02-22 17:23:26 +0900

    Threading and response to json

--- sound2text.py
+++ sound2text.py
@@ -1,15 +1,38 @@
+import threading
 import requests
 import json
-url = "https://kakaoi-newtone-openapi.kakao.com/v1/recognize"
-header = {"Content-Type": "application/octet-stream",
-          "Authorization" : "KakaoAK " + "5bdcd793f13bebf5e4e874d636b694c0"
-         }
 import speech_recognition as sr
-r = sr.Recognizer()
-with sr.Microphone(sample_rate=16000) as source:
-    print("말해보세요!!")
-    audio = r.listen(source)
-res = requests.post(url, headers=header, data=audio.get_raw_data())
-print(res.text)
+
+url = "https://kakaoi-newtone-openapi.kakao.com/v1/recognize"
+header = {
+        #"Transfer-Encoding: chunked"
+        "Content-Type": "application/octet-stream",
+        "Authorization" : "KakaoAK " + "5bdcd793f13bebf5e4e874d636b694c0"
+         }
+canRecording = True
+
+def record():
+    global canRecording
+    print("recording start")
+    r = sr.Recognizer()
+    with sr.Microphone(sample_rate=16000) as source:
+        audio = r.listen(source)
+    canRecording = True
+    res = requests.post(url, headers=header, data=audio.get_raw_data())
+    line = res.text.splitlines()
+    strRes = line[-2]
+    jsonRes = json.loads(strRes)
+    print('결과 : ' + jsonRes["value"])
+
+while True:
+    if canRecording == True:
+        t = threading.Thread(target=record)
+        t.start()
+        canRecording = False
+
+
+
+
+
 
 
